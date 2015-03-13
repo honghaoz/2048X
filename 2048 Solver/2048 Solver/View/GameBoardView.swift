11f2a8783c22c96141d4e63cfe1bd18ab978863d
Honghao Zhang <zhh358@gmail.com>
2015-03-13 11:13:30 -0400

    Added documentation for GameBoardView

--- 2048 Solver/2048 Solver/View/GameBoardView.swift
+++ 2048 Solver/2048 Solver/View/GameBoardView.swift
@@ -30,18 +30,6 @@ class GameBoardView: UIView {
     // TileWidth is updated automatically
     var tileWidth: CGFloat = 0.0
     
-//    override var backgroundColor: UIColor? {
-//        didSet {
-//            for i in 0 ..< dimension {
-//                for j in 0 ..< dimension {
-//                    if let tile = forgroundTiles[i][j].0 {
-//                        tile.backgroundColor = backgroundColor
-//                    }
-//                }
-//            }
-//        }
-//    }
-    
     /// Background tiles provide a grid like background layout
     var backgroundTiles = [[TileView]]()
     
@@ -124,32 +112,56 @@ class GameBoardView: UIView {
         }
     }
     
+    
+}
+
+// MARK: Update View Actions
+extension GameBoardView {
+    /**
+    When game model is updated, view controller should call this method and passed in corresponding MoveActions and InitActions
+    Note: this method will update MoveActions first and then InitActions
+    
+    :param: moveActions a list of actions which specify how tiles are merged or moved
+    :param: initActions a list of actions which specify how new tiles are inserted
+    */
     func updateWithMoveActions(moveActions: [MoveAction], initActions: [InitAction]) {
         updateWithMoveActions(moveActions, completion: {
             self.updateWithInitActions(initActions)
         })
     }
     
-    func updateWithInitActions(initActions: [InitAction]) {
+    /**
+    Insert new tile views using InitActions
+    
+    :param: initActions a list of actions which specify how new tiles are inserted
+    */
+    private func updateWithInitActions(initActions: [InitAction]) {
         for action in initActions {
-            let coordinate = action.initCoordinate
+            let initCoordinate = action.initCoordinate
             let number = action.initNumber
             
-            let tile = TileView(frame: tileFrameForCoordinate(coordinate))
+            let tile = TileView(frame: tileFrameForCoordinate(initCoordinate))
             tile.number = number
             
-            forgroundTiles[coordinate.0][coordinate.1].0 = tile
+            // Store new tile views
+            forgroundTiles[initCoordinate.0][initCoordinate.1].0 = tile
             self.addSubview(tile)
             
             // Blink pattern: 0 -> 1
             tile.alpha = 0.0
             UIView.animateWithDuration(sharedAnimationDuration * 2, animations: { () -> Void in
                 tile.alpha = 1.0
-            }, completion: nil)
+                }, completion: nil)
         }
     }
     
-    func updateWithMoveActions(moveActions: [MoveAction], completion: (() -> ())? = nil) {
+    /**
+    Move or merge tile views using MoveActions
+    
+    :param: moveActions a list of actions which specify how tiles are merged or moved
+    :param: completion  an optional completion clousure, which will be called once all move actions are done
+    */
+    private func updateWithMoveActions(moveActions: [MoveAction], completion: (() -> ())? = nil) {
         let count = moveActions.count
         // If there's no MoveActions, execute completion closure
         if count == 0 {
@@ -157,6 +169,7 @@ class GameBoardView: UIView {
             return
         }
         
+        // TODO: Use logger
         gameModel.printOutMoveActions(moveActions)
         
         for (index, action) in enumerate(moveActions) {
@@ -169,13 +182,16 @@ class GameBoardView: UIView {
                 // There may exist an underneath tile
                 let fromUnderneath = forgroundTiles[from.0][from.1].1
                 
+                // Set from tile to to tile and clean from tile
                 forgroundTiles[to.0][to.1] = forgroundTiles[from.0][from.1]
                 forgroundTiles[from.0][from.1] = (nil, nil)
-
+                
+                // Animation
                 UIView.animateWithDuration(sharedAnimationDuration, animations: { () -> Void in
                     fromView.frame = self.tileFrameForCoordinate(to)
                     fromUnderneath?.frame = fromView.frame
                     }, completion: { (finished) -> Void in
+                        // If this is the very last actions, call completion block
                         if index == count - 1 {
                             completion?()
                         }
@@ -186,6 +202,7 @@ class GameBoardView: UIView {
                 let from2 = action.fromCoordinates[1]
                 let to = action.toCoordinate
                 
+                // Make sure the inserting tile are under the inserted tile
                 self.insertSubview(forgroundTiles[from1.0][from1.1].0!, belowSubview: forgroundTiles[from2.0][from2.1].0!)
                 
                 let fromTileView = forgroundTiles[from1.0][from1.1].0!
@@ -200,14 +217,17 @@ class GameBoardView: UIView {
                     fromTileView.frame = self.tileFrameForCoordinate(to)
                     }, completion: { (finished) -> Void in
                         fromTileView.removeFromSuperview()
+                        
+                        // Double tile number and animate this tile
                         toTileView.number *= 2
                         toTileView.flashTile()
-                                                
+                        
+                        // If this is the very last actions, call completion block
                         if index == count - 1 {
                             dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
                                 Int64(sharedAnimationDuration * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), { () -> Void in
                                     completion?()
-
+                                    
                             })
                         }
                 })
@@ -218,6 +238,9 @@ class GameBoardView: UIView {
 
 // MARK: Property helpers
 extension GameBoardView {
+    /**
+    Update tileWidth when related properties are modified
+    */
     func updateTileWidth() {
         tileWidth = (width - padding * 2 - tilePadding * (CGFloat(dimension) - 1)) / CGFloat(dimension)
     }
@@ -225,6 +248,13 @@ extension GameBoardView {
 
 // MARK: Generic helpers
 extension GameBoardView {
+    /**
+    Return the tile frame for a tile coordinate
+    
+    :param: coordinate tile coordinate, within range [0 ..< dimension, 0 ..< dimension]
+    
+    :returns: CGRect frame
+    */
     func tileFrameForCoordinate(coordinate: (Int, Int)) -> CGRect {
         let y = padding + (tilePadding + tileWidth) * CGFloat(coordinate.0)
         let x = padding + (tilePadding + tileWidth) * CGFloat(coordinate.1)
