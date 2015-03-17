e22d89b6444e2ba0da3fc192ca53ba37b1dccd9e
LostZebra <xiaoyong19910227@gmail.com>
2015-03-17 01:05:27 -0400

    Minor minor minor changes

--- 2048 Solver/2048 Solver/AI/GameBoardAssistant.swift
+++ 2048 Solver/2048 Solver/AI/GameBoardAssistant.swift
@@ -88,7 +88,6 @@ class GameboardAssistant: Printable {
     // MARK: Deep copy
     func copy() -> GameboardAssistant {
         return GameboardAssistant(cells: _cell)
-<<<<<<< HEAD
     }
     
     /// Get the location of all empty cells
@@ -103,8 +102,6 @@ class GameboardAssistant: Printable {
         }
         
         return emptyCells
-=======
->>>>>>> origin/master
     }
     
     // MARK: Public methods
@@ -204,23 +201,6 @@ class GameboardAssistant: Printable {
         return getAllEmptyCells().count == 0;
     }
     
-<<<<<<< HEAD
-=======
-    /// Get the location of all empty cells
-    func getAllEmptyCells() -> [(x: Int, y: Int)] {
-        var emptyCells: [(x: Int, y: Int)] = []
-        for i in 0..<_size {
-            for j in 0..<_size {
-                if _cell[j][i] == 0 {
-                    emptyCells.append(x: i, y: j)
-                }
-            }
-        }
-        
-        return emptyCells
-    }
-    
->>>>>>> origin/master
     /// Get a copy of certain row
     private func getRow(x: Int) -> [Int] {
         return _cell[x]
