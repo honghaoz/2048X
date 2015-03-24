97b364391f99a2091dbf3836dd29519761fd145d
Honghao Zhang <zhh358@gmail.com>
2015-03-23 23:45:31 -0400

    Added AIOperation
    Former-commit-id: f28f4cc3cf12bebd454d49ff0b81d648e36440bc

---
+++ 2048 Solver/2048 Solver/AIOperation.swift
@@ -0,0 +1,13 @@
+//
+//  AIOperation.swift
+//  2048 Solver
+//
+//  Created by Honghao Zhang on 3/23/15.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+import Foundation
+
+class AIOperation: NSOperation {
+    
+}
