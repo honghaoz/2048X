a43c02eb4bcc133ac5e6a2682783dc4a69c948d4
yansong li <857367901@qq.com>
2015-03-29 15:15:32 -0400

    add Files to Directory
    Former-commit-id: 1afe3f7bf00158a2a043a4841547a1970f4523ba

---
+++ 2048 Solver/2048 Solver/TDLearning/RectSize.swift
@@ -0,0 +1,45 @@
+//
+//  RectSize.swift
+//  2048game
+//
+//  Created by yansong li on 2015-03-26.
+//  Copyright (c) 2015 yansong li. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct RectSize :Equatable{
+    let columns:Int
+    let rows:Int
+    
+    var width:Int {
+        get{
+            return columns
+        }
+    }
+    
+    var height:Int {
+        get{
+            return rows
+        }
+    }
+    
+    init(rows:Int, cols:Int) {
+        self.rows = rows
+        self.columns = cols
+    }
+    
+    init(size:Int) {
+        self.init(rows:size, cols:size)
+    }
+    
+}
+
+func == (lhs: RectSize, rhs:RectSize) -> Bool {
+    if (lhs.columns == rhs.columns && lhs.rows == rhs.rows) {
+        return true
+    }
+    return false
+}
