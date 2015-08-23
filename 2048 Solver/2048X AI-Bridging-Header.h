a77726da6d725a0f9c205026cea0376684ba510c
Honghao Zhang <zhh358@gmail.com>
2015-08-22 23:13:03 -0400

    Cleaned up
    Former-commit-id: 17fd8bb45457ec524f90042b5630e50cf1246149

--- 2048 Solver/2048X AI-Bridging-Header.h
+++ 2048 Solver/2048X AI-Bridging-Header.h
@@ -6,13 +6,4 @@
 
 // Analytics
 #import "ZHHParseDevice.h"
-//#import <ARAnalytics/ARAnalytics.h>
-#import <GoogleAnalytics-iOS-SDK/GAI.h>
-#import <GoogleAnalytics-iOS-SDK/GAIDictionaryBuilder.h>
-#import <GoogleAnalytics-iOS-SDK/GAIEcommerceProduct.h>
-#import <GoogleAnalytics-iOS-SDK/GAIEcommerceProductAction.h>
-#import <GoogleAnalytics-iOS-SDK/GAIEcommercePromotion.h>
-#import <GoogleAnalytics-iOS-SDK/GAIFields.h>
-#import <GoogleAnalytics-iOS-SDK/GAILogger.h>
-#import <GoogleAnalytics-iOS-SDK/GAITrackedViewController.h>
-#import <GoogleAnalytics-iOS-SDK/GAITracker.h>
+#import <Google/Analytics.h>
